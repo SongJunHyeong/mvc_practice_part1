04c1bbfc0bfa97f91ff394f72802c6cf86348289
SongJunHyeong <thdwnsgud79@naver.com>
2024-03-10 01:12:26 +0900

    1일차

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -8,12 +8,21 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name = "helloServlet", urlPatterns = "/hello")  //이름은 아무거나 지어도 됨. 클래스 명과 비슷한걸 선호 , /hello로 오면 HelloServlet이 실행되는것이다.
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")  //이름은 아무거나 지어도 됨. 클래스 명과 비슷한걸 선호 , /hello로 오면 HelloServlet이 실행되는것이다. + 둘이 겹치면 안된다.
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
     }
 }
