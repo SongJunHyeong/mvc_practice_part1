0e20b7f962aee6674cbd5b26ba01ac5668ff3437
SongJunHyeong <thdwnsgud79@naver.com>
2024-03-09 19:17:59 +0900

    커밋 테스트

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,19 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")  //이름은 아무거나 지어도 됨. 클래스 명과 비슷한걸 선호 , /hello로 오면 HelloServlet이 실행되는것이다.
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+    }
+}
